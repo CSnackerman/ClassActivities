75f7a7958093c64857c8b3d81c73f6542287087e
ca <colbyackerman@gmail.com>
2024-07-09 19:52:10 -0500

    Function Declarations

---
+++ Section03/3.1/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,16 @@
+function greet() {
+  console.log('Hello!');
+}
+
+function threeModTwo() {
+  console.log(3 % 2);
+}
+
+let age = 18;
+function checkDrivingAge() {
+  if (age >= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
